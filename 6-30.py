a8755ba9a26527e18c1f4d5f58ceab3bdb9671c3
yuta2001 <ymaiiyuta0424@gmail.com>
2023-06-30 14:22:11 +0900

    format途中

--- 6-30.py
+++ 6-30.py
@@ -1,3 +1,6 @@
+import math
+
+
 #print("first commit")
 
 
@@ -12,3 +15,22 @@ new_num = int(num)
 
 print(num,type(num))
 print(new_num,type(new_num))
+
+#sep=","で区切る
+print("hi","yuta",sep=",",end=".\n")
+
+#end=""はデフォルトで改行
+print("hi","yuta",sep=",",end="\n")
+print("---------------------------------")
+
+result = math.sqrt(25)
+print(result,type(result),end=".\n")
+
+
+print("""
+    line1
+    line2
+    line3
+""" )
+
+print("my name is {0} {1}" .format("yuta","matsui"))
