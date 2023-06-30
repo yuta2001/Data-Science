ebf2a004b4d6b8ba4b68572152aedcdb24a3c2bb
yuta2001 <ymaiiyuta0424@gmail.com>
2023-06-30 12:16:39 +0900

    python色んな型変換

--- 6-30.py
+++ 6-30.py
@@ -1,1 +1,14 @@
-print("first commit")
+#print("first commit")
+
+
+the_world_python3 = True
+if the_world_python3:
+    print("hogehoge")
+    
+num = "1"
+name = "yuta"
+
+new_num = int(num)
+
+print(num,type(num))
+print(new_num,type(new_num))
